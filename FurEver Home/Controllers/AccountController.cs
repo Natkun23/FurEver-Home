af6a4e6407ce796e7b8b951dc4fbb4a578290706
Natkun23 <batallones.juannathaniel.kho@gmail.com>
2025-11-16 18:46:02 +0800

    LOGIN AND SIGNUP (FRONTEND)

---
+++ FurEver Home/Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+﻿using System.Web.Mvc;
+using FurEver_Home.Models;
+
+namespace FurEver_Home.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Add your authentication logic here
+                // Example: Check if user exists in database
+                // if (IsValidUser(model.Email, model.Password))
+                // {
+                //     FormsAuthentication.SetAuthCookie(model.Email, model.RememberMe);
+                //     return RedirectToAction("Index", "Home");
+                // }
+
+                // For now, just show a success message
+                TempData["Success"] = "Login functionality will be implemented!";
+                return RedirectToAction("Index", "Home");
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        // GET: Account/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Add your registration logic here
+                // Example: Create new user in database
+                // var user = new User
+                // {
+                //     FullName = model.FullName,
+                //     Email = model.Email,
+                //     Password = HashPassword(model.Password)
+                // };
+                // db.Users.Add(user);
+                // db.SaveChanges();
+
+                // For now, redirect to login
+                TempData["Success"] = "Account created successfully! Please login.";
+                return RedirectToAction("Login");
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        // GET: Account/Logout
+        public ActionResult Logout()
+        {
+            // TODO: Add logout logic
+            // FormsAuthentication.SignOut();
+            TempData["Success"] = "You have been logged out.";
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ FurEver Home/Models/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FurEver_Home.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Full name is required")]
+        [Display(Name = "Full Name")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, ErrorMessage = "Password must be at least {2} characters long", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
